81dea714328373ee5e4507c600b5ba2dc78ca702
thang55555 <bsthang9x@gmail.com>
2026-07-16 07:07:38 +0700

    Update share.js

--- src/apps/middlewares/share.js
+++ src/apps/middlewares/share.js
@@ -6,6 +6,7 @@ const Menu_nhom_sanphamModel = require("../models/menu_nhom_sanpham");
 module.exports = async (req, res, next)=>{
     res.locals.menudv = await Menu_dichvuModel.find({ web: "Nhagonamthanhphat.com" });
     res.locals.menutt = await Menu_tintucModel.find({ web: "Nhagonamthanhphat.com" });
+    
     res.locals.thongtintrang = await Thong_tin_trangModel.find();
     const menu = await Menu_danhmuc_sanphamModel.find({ web: "Nhagonamthanhphat.com" });
     const MENU=[];
